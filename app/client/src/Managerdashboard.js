140b23bec11371740fdbd1e1a472a6984e2a9f05
burak <rburaksaritas@gmail.com>
2023-05-23 02:35:41 +0300

    Modify manager-dashboard tabs layout

--- app/client/src/Managerdashboard.js
+++ app/client/src/Managerdashboard.js
@@ -441,186 +441,212 @@ function Managerdashboard() {
     const renderContent = () => {
         if (currentTab === 'audience') {
             return (
-                <div>
-                    <h2>Add Audience</h2>
-                    <form onSubmit={handleAddAudience}>
-                        <label>User Name:</label>
-                        <input
-                            type='text'
-                            value={newAudience.user_name}
-                            onChange={(e) =>
-                                setNewAudience({ ...newAudience, user_name: e.target.value })
-                            }
-                        />
-                        <label>Audience Password:</label>
-                        <input
-                            type='password'
-                            value={newAudience.audience_password}
-                            onChange={(e) =>
-                                setNewAudience({ ...newAudience, audience_password: e.target.value })
-                            }
-                        />
-                        <label>Audience Name:</label>
-                        <input
-                            type='text'
-                            value={newAudience.audience_name}
-                            onChange={(e) =>
-                                setNewAudience({ ...newAudience, audience_name: e.target.value })
-                            }
-                        />
-                        <label>Audience Surname:</label>
-                        <input
-                            type='text'
-                            value={newAudience.audience_surname}
-                            onChange={(e) =>
-                                setNewAudience({ ...newAudience, audience_surname: e.target.value })
-                            }
-                        />
-                        <button type='submit' className='button'>Add Audience</button>
-                    </form>
+                <div className='large-container'>
+                    <div className='form-container'>
+                        <h2>Add Audience</h2>
+                        <form onSubmit={handleAddAudience}>
+                            <label>User Name:</label>
+                            <input
+                                type="text"
+                                value={newAudience.user_name}
+                                onChange={(e) =>
+                                    setNewAudience({ ...newAudience, user_name: e.target.value })
+                                }
+                            />
+                            <label>Audience Password:</label>
+                            <input
+                                type="password"
+                                value={newAudience.audience_password}
+                                onChange={(e) =>
+                                    setNewAudience({ ...newAudience, audience_password: e.target.value })
+                                }
+                            />
+                            <label>Audience Name:</label>
+                            <input
+                                type="text"
+                                value={newAudience.audience_name}
+                                onChange={(e) =>
+                                    setNewAudience({ ...newAudience, audience_name: e.target.value })
+                                }
+                            />
+                            <label>Audience Surname:</label>
+                            <input
+                                type="text"
+                                value={newAudience.audience_surname}
+                                onChange={(e) =>
+                                    setNewAudience({ ...newAudience, audience_surname: e.target.value })
+                                }
+                            />
+                            <button type="submit" className="button">
+                                Add Audience
+                            </button>
+                        </form>
 
-                    <h2>Delete Audience</h2>
-                    <form onSubmit={handleDeleteAudience}>
-                        <label>User Name:</label>
-                        <input
-                            type='text'
-                            value={deleteAudience}
-                            onChange={(e) => setDeleteAudience(e.target.value)}
-                        />
-                        <button type='submit' className='button'>Delete Audience</button>
-                    </form>
-
-                    <h2>Current Audiences</h2>
-                    {renderAudienceTable()}
+                        <h2>Delete Audience</h2>
+                        <form onSubmit={handleDeleteAudience}>
+                            <label>User Name:</label>
+                            <input
+                                type="text"
+                                value={deleteAudience}
+                                onChange={(e) => setDeleteAudience(e.target.value)}
+                            />
+                            <button type="submit" className="button">
+                                Delete Audience
+                            </button>
+                        </form>
+                    </div>
+                    <div className='results-container'>
+                        <h2>Current Audiences</h2>
+                            {renderAudienceTable()}
+                    </div>
                 </div>
             );
         } else if (currentTab === 'directors') {
             return (
-                <div>
-                    <h2>Add Director</h2>
-                    <form onSubmit={handleAddDirector}>
-                        <label>User Name:</label>
-                        <input
-                            type='text'
-                            value={newDirector.user_name}
-                            onChange={(e) =>
-                                setNewDirector({ ...newDirector, user_name: e.target.value })
-                            }
-                        />
-                        <label>Director Password:</label>
-                        <input
-                            type='password'
-                            value={newDirector.director_password}
-                            onChange={(e) =>
-                                setNewDirector({ ...newDirector, director_password: e.target.value })
-                            }
-                        />
-                        <label>Director Name:</label>
-                        <input
-                            type='text'
-                            value={newDirector.director_name}
-                            onChange={(e) =>
-                                setNewDirector({ ...newDirector, director_name: e.target.value })
-                            }
-                        />
-                        <label>Director Surname:</label>
-                        <input
-                            type='text'
-                            value={newDirector.director_surname}
-                            onChange={(e) =>
-                                setNewDirector({ ...newDirector, director_surname: e.target.value })
-                            }
-                        />
-                        <label>Nationality:</label>
-                        <input
-                            type='text'
-                            value={newDirector.nationality}
-                            onChange={(e) =>
-                                setNewDirector({ ...newDirector, nationality: e.target.value })
-                            }
-                        />
-                        <label>Platform ID:</label>
-                        <input
-                            type='text'
-                            value={newDirector.platform_id}
-                            onChange={(e) =>
-                                setNewDirector({ ...newDirector, platform_id: e.target.value })
-                            }
-                        />
-                        <button type='submit' className='button'>Add Director</button>
-                    </form>
+                <div className='large-container'>
+                    <div className='form-container'>
+                        <h2>Add Director</h2>
+                        <form onSubmit={handleAddDirector}>
+                            <label>User Name:</label>
+                            <input
+                                type="text"
+                                value={newDirector.user_name}
+                                onChange={(e) =>
+                                    setNewDirector({ ...newDirector, user_name: e.target.value })
+                                }
+                            />
+                            <label>Director Password:</label>
+                            <input
+                                type="password"
+                                value={newDirector.director_password}
+                                onChange={(e) =>
+                                    setNewDirector({ ...newDirector, director_password: e.target.value })
+                                }
+                            />
+                            <label>Director Name:</label>
+                            <input
+                                type="text"
+                                value={newDirector.director_name}
+                                onChange={(e) =>
+                                    setNewDirector({ ...newDirector, director_name: e.target.value })
+                                }
+                            />
+                            <label>Director Surname:</label>
+                            <input
+                                type="text"
+                                value={newDirector.director_surname}
+                                onChange={(e) =>
+                                    setNewDirector({ ...newDirector, director_surname: e.target.value })
+                                }
+                            />
+                            <label>Nationality:</label>
+                            <input
+                                type="text"
+                                value={newDirector.nationality}
+                                onChange={(e) =>
+                                    setNewDirector({ ...newDirector, nationality: e.target.value })
+                                }
+                            />
+                            <label>Platform ID:</label>
+                            <input
+                                type="text"
+                                value={newDirector.platform_id}
+                                onChange={(e) =>
+                                    setNewDirector({ ...newDirector, platform_id: e.target.value })
+                                }
+                            />
+                            <button type="submit" className="button">
+                                Add Director
+                            </button>
+                        </form>
 
-                    <h2>Change Platform ID</h2>
-                    <form onSubmit={handleChangeDirectorPlatform}>
-                        <label>User Name:</label>
-                        <input
-                            type='text'
-                            value={changeDirectorPlatform.user_name}
-                            onChange={(e) =>
-                                setChangeDirectorPlatform({ ...changeDirectorPlatform, user_name: e.target.value })
-                            }
-                        />
-                        <label>Platform ID:</label>
-                        <input
-                            type='text'
-                            value={changeDirectorPlatform.platform_id}
-                            onChange={(e) =>
-                                setChangeDirectorPlatform({ ...changeDirectorPlatform, platform_id: e.target.value })
-                            }
-                        />
-                        <button type='submit' className='button'>Change Platform ID</button>
-                    </form>
-
-                    <h2>Current Directors</h2>
-                    {renderDirectorTable()}
+                        <h2>Change Platform ID</h2>
+                        <form onSubmit={handleChangeDirectorPlatform}>
+                            <label>User Name:</label>
+                            <input
+                                type="text"
+                                value={changeDirectorPlatform.user_name}
+                                onChange={(e) =>
+                                    setChangeDirectorPlatform({ ...changeDirectorPlatform, user_name: e.target.value })
+                                }
+                            />
+                            <label>Platform ID:</label>
+                            <input
+                                type="text"
+                                value={changeDirectorPlatform.platform_id}
+                                onChange={(e) =>
+                                    setChangeDirectorPlatform({ ...changeDirectorPlatform, platform_id: e.target.value })
+                                }
+                            />
+                            <button type="submit" className="button">
+                                Change Platform ID
+                            </button>
+                        </form>
+                    </div>
+                    <div className='results-container'>
+                        <h2>Current Directors</h2>
+                            {renderDirectorTable()}
+                    </div>
                 </div>
             );
         } else if (currentTab === 'ratings') {
             return (
-                <div>
+                <div className='content-container'>
                     <h2>Search Ratings</h2>
                     <form onSubmit={handleRatingSearch}>
                         <label>Audience User Name:</label>
                         <input
-                            type='text'
+                            type="text"
                             value={ratingSearch}
                             onChange={(e) => setRatingSearch(e.target.value)}
                         />
-                        <button type='submit' className='button'>Search Ratings</button>
+                        <button type="submit" className="button">
+                            Search Ratings
+                        </button>
                     </form>
-                    {renderRatingResults()}
+                    <div className='results-container'>
+                        {renderRatingResults()}
+                    </div>
                 </div>
             );
         } else if (currentTab === 'movies') {
             return (
-                <div>
+                <div className='content-container'>
                     <h2>Search Movies</h2>
                     <form onSubmit={handleMovieSearch}>
                         <label>Director User Name:</label>
                         <input
-                            type='text'
+                            type="text"
                             value={movieSearch}
                             onChange={(e) => setMovieSearch(e.target.value)}
                         />
-                        <button type='submit' className='button'>Search Movies</button>
+                        <button type="submit" className="button">
+                            Search Movies
+                        </button>
                     </form>
-                    {renderMovieResults()}
+                    <div className='results-container'>
+                        {renderMovieResults()}
+                    </div>
                 </div>
             );
         } else if (currentTab === 'average-rating') {
             return (
-                <div>
+                <div className='content-container'>
                     <h2>Get Average Rating</h2>
                     <form onSubmit={handleGetAverageRating}>
                         <label>Movie ID: </label>
                         <input
-                            type='text'
+                            type="text"
                             value={movieId}
                             onChange={(e) => setMovieId(e.target.value)}
                         />
-                        <button type='submit' className='button'>Submit</button>
+                        <button type="submit" className="button">
+                            Get Average Rating
+                        </button>
                     </form>
-                    {renderAverageRating()}
+                    <div className='results-container'>
+                        {renderAverageRating()}
+                    </div>
                 </div>
             );
         } else {
@@ -639,7 +665,7 @@ function Managerdashboard() {
                 <button onClick={() => handleTabClick('average-rating')} className='button'>Average Rating</button>
                 <button onClick={() => handleRedirectToLogin()} className='button' id='logout-button'>Logout</button>
             </div>
-            <div className='content-container'>{renderContent()}</div>
+            {renderContent()}
         </div>
     );
 }
